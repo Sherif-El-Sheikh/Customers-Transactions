b8e3a2728d100cb7030bebd65a4591e008e8990b
Sherif-El-Sheikh <shreefsheikh99@gmail.com>
2024-07-14 01:03:47 +0300

    fixed build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import './App.css'
 import { Customers } from '@/components/Customers/Customers'
-import { CustomersChart } from '@/components/FlowChart/CustomersChart'
+
 
 function App() {
-  
-
   return (
     <>
     <Customers/>
-    {/* <CustomersChart/> */}
     </>
   )
 }
